ff5e95b3bd35a14cf44b3c677d9955436e2eb633
YanisRichard <yanis.richard17@gmail.com>
2024-03-25 13:55:01 +0100

    affichage et lien fonctionnel pour les coffrets

--- src/components/AfficheSingle.vue
+++ src/components/AfficheSingle.vue
@@ -16,15 +16,25 @@ defineProps<Database['public']['Tables']['Film']['Insert'] & {Collection:Tables<
 
 <template>
 
-    <div class="w-[300px] ml-20 mt-10 grid-flow-row">
-      <img class="w-[200px] h-[300px]  " :src="image_film" :alt="nom"/>
-      <p class="pt-5">{{ nom }}</p>
-      <p class="pt-5">Année de sortie: {{ annee_sortie }}</p>
-      <p class="pt-5">Durée du film: {{ duree }}</p>
-      <p class="pt-5">Classification Européenne: {{ classification }}</p>
-      <p class="pt-5 pb-5">Synopsis:  {{ synopsis }}</p>
-      <p class="pt-5 pb-5">Collection:  {{ Collection.nom_coffret }}</p>
+    <div class=" mt-10 flex flex-auto flex-row">
+      <div class="ml-20 w-[20%]">
+        <img  :src="image_film" :alt="nom"/>
+      </div>
+      <div class="ml-10 w-[50%]">
+        <p class="pt-5">{{ nom }}</p>
+        <p class="pt-5">Année de sortie: {{ annee_sortie }}</p>
+        <p class="pt-5">Durée du film: {{ duree }}</p>
+        <p class="pt-5">Classification Européenne: {{ classification }}</p>
+        <p class="pt-5 pb-5">Synopsis:  {{ synopsis }}</p>
+      </div>
     </div>
+
+    <div class="flex ml-10" >
+        <p class="pt-5 pb-5">Collection:  {{ Collection.nom_coffret }}</p>
+        <a :href="Collection.lien_coffret" class="pt-5 pb-5 ml-11 text-blue-600">Acheter le coffret</a>
+    </div>
+
+    
     
     
     </template>
